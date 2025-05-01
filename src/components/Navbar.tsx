620e1414227bd662cd7dd5cf43efb2b42ed69b48
mateusmatosleonardo <m4teusmatos@gmail.com>
2025-04-30 20:15:57 -0400

    feat: add new elements and links in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,42 +1,67 @@
-import { auth, signIn, signOut } from "auth";
+import Image from "next/image";
+import { auth, signOut } from "auth";
 import Link from "next/link";
+import { getUserByEmail } from "@/actions";
+import { FaPlusCircle } from "react-icons/fa";
+import Button from "./Button";
+import ButtonLink from "./ButtonLink";
 
 export default async function Navbar() {
   const session = await auth();
 
+  const user = await getUserByEmail(session?.user.email);
+
   return (
     <div className="bg-gray-800 text-white p-4 flex justify-between items-center">
-      <Link href="/" className="text-white text-lg font-bold">
-        Início
+      <Link
+        href="/"
+        className="text-white hover:text-zinc-200 text-lg font-bold"
+      >
+        Postilo
       </Link>
       <div>
-        {session ? (
+        {user ? (
           <div className="flex gap-4 items-center">
-            <p>{session.user?.name}</p>
+            <Link
+              href="/post-new"
+              className="font-medium text-white hover:text-zinc-200"
+            >
+              <FaPlusCircle className="text-white" size={20} />
+            </Link>
+            <Link
+              href="/profile"
+              className="font-medium text-white hover:text-zinc-200"
+            >
+              Perfil
+            </Link>
+            <Link
+              href="/my-posts"
+              className="font-medium text-white hover:text-zinc-200"
+            >
+              Meus posts
+            </Link>
+            <p className="font-medium text-white">{user.name}</p>
+            {user.image && (
+              <Image
+                src={user.image}
+                alt={`perfil de: ${user.name}`}
+                className="w-10 h-10 rounded-full"
+                width={40}
+                height={40}
+              />
+            )}
             <form
               action={async () => {
                 "use server";
                 await signOut();
               }}
             >
-              <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded">
-                Sair
-              </button>
+              <Button text="Sair" danger={true} type="submit" />
             </form>
           </div>
         ) : (
-          <form
-          // action={async () => {
-          //   "use server";
-          //   await signIn("google", { callbackUrl: "/" });
-          // }}
-          >
-            <Link
-              href="/sign-in"
-              className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded"
-            >
-              Entrar
-            </Link>
+          <form>
+            <ButtonLink text="Entrar" href="/sign-in" />
           </form>
         )}
       </div>
