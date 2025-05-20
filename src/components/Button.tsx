11035817d2a95a7406219bf18564087a1c0a89d4
mateusmatosleonardo <m4teusmatos@gmail.com>
2025-05-20 19:41:12 -0400

    feat: add use form status in button to disabled button and show loading

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
-import { ComponentProps } from "react";
+"use client";
+
+import { ComponentProps, ReactNode } from "react";
+import { LuLoader } from "react-icons/lu";
+import { useFormStatus } from "react-dom";
 
 type ButtonProps = ComponentProps<"button"> & {
-  text: string;
+  text: string | ReactNode;
   danger?: boolean;
 };
 
@@ -10,13 +14,17 @@ export default function Button({
   danger = false,
   ...props
 }: ButtonProps) {
+  const { pending } = useFormStatus();
+
   return (
     <button
-      className="flex items-center bg-blue-800 hover:bg-blue-700 h-8 text-sm font-medium py-1 px-6 rounded text-white data-[danger=true]:bg-red-500 data-[danger=true]:hover:bg-red-400"
+      className="flex items-center bg-blue-800 hover:bg-blue-700 h-8 text-sm font-medium py-1 px-6 gap-1 rounded text-white data-[danger=true]:bg-red-500 data-[danger=true]:hover:bg-red-400"
       data-danger={danger}
+      disabled={pending || props.disabled}
       {...props}
     >
       {text}
+      {pending && <LuLoader className="h-4 w-4 animate-spin" />}
     </button>
   );
 }
